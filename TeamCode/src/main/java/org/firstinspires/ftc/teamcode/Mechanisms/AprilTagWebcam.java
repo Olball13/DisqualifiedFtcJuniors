4a44721ba697fc24051b2a30a5053d2575d6d76d
NeeksR12 <500009816@dclive.ca>
2026-04-30 13:07:19 -0400

    Put the Crash Webcam into this webcam, full replacement and to be used for merge only HAS ERRORS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/AprilTagWebcam.java
@@ -1,170 +1,176 @@
-package org.firstinspires.ftc.teamcode.Mechanisms;
+package org.firstinspires.ftc.teamcode;
 
-import android.util.Size;
 
-import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.teamcode.Constants.AprilTagWebcamConstants;
-import org.firstinspires.ftc.teamcode.Teleops.ErrorMainTeleop;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+
+import android.util.Size;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class AprilTagWebcam {
+
     private AprilTagProcessor aprilTagProcessor;
     private VisionPortal visionPortal;
-    private List<AprilTagDetection> detectedTags = new ArrayList<>();
-    private Telemetry telemetry;
-    private double degreeCorrection;
-    private double rangeCorrection;
-    public Pose pedroPoseCamCorrection;
+    private List<AprilTagDetection> aprilTagDetection = new ArrayList<>();
 
-    public void init(HardwareMap hardwareMap, Telemetry telemetry) {
+    // Store references
+    private Telemetry telemetry;
+
+
+    /**
+     * Description: Initializes all of the required April Tag Processors and Vision Portal
+     * Pre-Condition: Must be given a hardware map with the correct names and a telemetry object
+     * Post-Condition: The camera is initialized
+     * @param hwMap The hardware map
+     * @param tel The telemetry
+     */
+    public void init(HardwareMap hwMap, Telemetry tel) {
+        this.telemetry = tel;
+
         aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
                 .setDrawTagID(true)
                 .setDrawTagOutline(true)
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
-                .setOutputUnits(DistanceUnit.INCH, AngleUnit.RADIANS)
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
                 .build();
-        aprilTagProcessor.setDecimation(2);
+
         VisionPortal.Builder builder = new VisionPortal.Builder();
-        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCamera(hwMap.get(WebcamName.class, "Webcam 1"));
         builder.setCameraResolution(new Size(640, 480));
-        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
         builder.addProcessor(aprilTagProcessor);
+
         visionPortal = builder.build();
-        while (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
-            telemetry.addLine("...");
-        }
-        ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
-        exposureControl.setMode(ExposureControl.Mode.Manual);
-        exposureControl.setExposure(15, TimeUnit.MILLISECONDS);
-        GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
-        gainControl.setGain(20);
-        this.telemetry = telemetry;
     }
 
-    public void update(double lazySusanOrientation) {
-        detectedTags = aprilTagProcessor.getDetections();
-        fieldRelativeUpdate(lazySusanOrientation);
-        aprilTagBasketDetection();
+    /**
+     * Description: Updates what the camera is seeing to the detection
+     * Pre-Condition: Objects are declared and initialized
+     * Post-Condition: Camera is updated
+     */
+    public void update() {
+        aprilTagDetection = aprilTagProcessor.getDetections();
     }
 
-    public List<AprilTagDetection> getDetectedTags() {
-        return detectedTags;
-    }
-
-    public void displayDetectionTelemetry(AprilTagDetection detectedid) {
-        if (detectedid == null) {
-            return;}
-
-        if (detectedid.metadata != null) {
-            telemetry.addData("Degree Correction", degreeCorrection);
-            telemetry.addData("Range Correction", rangeCorrection);
-            telemetry.addLine("----Detected Tags----");
-            telemetry.addLine(getDetectedTags().toString());
-        } else {
-            telemetry.addLine("Unknown Id");
-            telemetry.addData("Info", detectedid.center.x + ", " + detectedid.center.y);
-        }
-
-    }
-
-    public AprilTagDetection getTagBySpecificid(int id) {
-        for (AprilTagDetection detection : detectedTags) {
+    /**
+     * Description: Checks for a specific tag
+     * Pre-Condition: All objects are declared and initialized
+     * Post-Condition: Returns tag detection info if found or else returns null
+     * @param id The tag ID number
+     * @return The tag information (null if unsuccessful)
+     */
+    public AprilTagDetection getTagBySpecificId(int id) {
+        for (AprilTagDetection detection : aprilTagDetection) {
             if (detection.id == id) {
-                degreeCorrection = -detection.ftcPose.bearing;
-                rangeCorrection = detection.ftcPose.range;
                 return detection;
             }
         }
-        degreeCorrection = 0;
-        rangeCorrection = 0;
         return null;
     }
 
-    private void fieldRelativeUpdate( double lazySusanOrientation) {
-        //the robot x and y are converted from FTC to Pedro coordinates by adding 72 inches
-        if (detectedTags == null || detectedTags.isEmpty()) { //Check if there are any detections
-            pedroPoseCamCorrection = null;
+    /**
+     * Description: Adds the camera information to the telemetry
+     * Pre-Condition: All objects and hardware are declared and initialized
+     * Post-Condition: The information is sent to the telemetry
+     * @param detectedId The ID that is detected
+     */
+    public void displayDetectionTelemetry(AprilTagDetection detectedId) {
+        if (detectedId == null) {
+            telemetry.addData("Tag", "Not detected");
             return;
         }
-        double totalX = 0; //Sum of all x, y, sin, and cos detections
-        double totalY = 0;
-        double totalSin = 0;
-        double totalCos = 0;
-        int tags = 0; //How many tags info detected
-        for (AprilTagDetection detection : detectedTags) {
-            if(detection.metadata == null) {continue;}
-            double tagfieldx = detection.metadata.fieldPosition.get(0); //field relative x of tag
-            double tagfieldy = detection.metadata.fieldPosition.get(1); //field relative y of tag
-            Orientation tagAngles = Orientation.getOrientation(
-                    detection.metadata.fieldOrientation.toMatrix(),
-                    AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.RADIANS); //Conversion from Quaternion to Orientation values
-            double tagfieldheading = tagAngles.thirdAngle; //field relative heading of tag
-            double range = detection.ftcPose.range; //range from cam to tag
-            double bearing = detection.ftcPose.bearing; //degree of deflection of cam from tag
-            double turretangle = lazySusanOrientation; //heading of the turret/cam
-            double camfieldheading = tagfieldheading - bearing; //field relative heading of the turret/cam
-            double camfieldx = tagfieldx - (range * Math.cos(camfieldheading)); //field relative x of cam
-            double camfieldy = tagfieldy - (range * Math.sin(camfieldheading)); //field relative y of cam
-            double robotheading = camfieldheading - turretangle; //field relative heading of robot
-            double turretx = camfieldx - (AprilTagWebcamConstants.camRadius * Math.cos(camfieldheading)); //field relative x of turret
-            double turrety = camfieldy - (AprilTagWebcamConstants.camRadius * Math.sin(camfieldheading)); //field relative y of turret
-            double offsetx = AprilTagWebcamConstants.camXCorrection * Math.cos(robotheading) - AprilTagWebcamConstants.camYCorrection * Math.sin(robotheading); //Rotation matrix math
-            double offsety = AprilTagWebcamConstants.camXCorrection * Math.sin(robotheading) + AprilTagWebcamConstants.camYCorrection * Math.cos(robotheading); //Converts robot relative offset to field relative offset
-            double robotx = turretx - offsetx + 72; //Pedro field relative x of robot
-            double roboty = turrety - offsety + 72; //Pedro field relative y of robot
-            totalSin += Math.sin(robotheading); //Add detection data to total values
-            totalCos += Math.cos(robotheading);
-            totalX += robotx;
-            totalY += roboty;
-            tags++; //Add to the # of tags detected
+
+        if (detectedId.metadata != null) {
+            telemetry.addData("Tag ID", detectedId.id);
+            telemetry.addData("Range (in)", "%.1f", detectedId.ftcPose.range);
+            telemetry.addData("Bearing (deg)", "%.1f", detectedId.ftcPose.bearing);
+            telemetry.addData("Yaw (deg)", "%.1f", detectedId.ftcPose.yaw);
         }
-        double averageHeading = Math.atan2(totalSin, totalCos); //converting into radian value and automatically averaging
-        double averageX = totalX / tags; //average x of all detections
-        double averageY = totalY/ tags; //average y of all detections
-        pedroPoseCamCorrection = new Pose (averageX, averageY, averageHeading); //updating the cam correction
+        else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detectedId.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f (pixels)", detectedId.center.x, detectedId.center.y));
+        }
+
     }
 
-    private void aprilTagBasketDetection() {
-        if (ErrorMainTeleop.alliance.equals("Blue")) {
-            AprilTagDetection id20 = getTagBySpecificid(20);
-            displayDetectionTelemetry(id20);
-        } else {
-            AprilTagDetection id24 = getTagBySpecificid(24);
-            displayDetectionTelemetry(id24);
-        }
-    }
-
+    /**
+     * Description: Closes the vision portal and stops the webcam
+     * Pre-Condition: All hardware and objects are initialized
+     * Post-Condition: If the vision portal is not already null, it is closed
+     */
     public void stop() {
         if (visionPortal != null) {
             visionPortal.close();
         }
     }
 
-
-
-    // Getters and Setters
-    public double getDegreeCorrection() {
-        return degreeCorrection;
+    /**
+     * Description: Returns the distance from a detected tag (-1 if not found)
+     * Pre-Condition: All hardware and objects are declared and initialized
+     * Post-Condition: Distance is returned in inches (-1 if not found)
+     * @param tagId The ID being detected
+     * @return The distance in inches
+     */
+    public double getDistanceFromId(int tagId) {
+        if (getTagBySpecificId(tagId) == null)
+            return -1;
+        else
+            return getTagBySpecificId(tagId).ftcPose.range;
     }
-    public double getRangeCorrection() {
-        return rangeCorrection;
+
+    /**
+     * Description: Returns the distance from a detected tag (0 if not found)
+     * Pre-Condition: All hardware and objects are declared and initialized
+     * Post-Condition: Distance is returned in inches (0 if not found)
+     * @param tagId The ID being detected
+     * @return The bearing in degrees
+     */
+    public double getBearingFromId(int tagId) {
+        if (getTagBySpecificId(tagId) == null)
+            return 0;
+        else
+            return getTagBySpecificId(tagId).ftcPose.bearing;
     }
+
+    /**
+     * Description: Returns the distance from a detected tag (0 if not found)
+     * Pre-Condition: All hardware and objects are declared and initialized
+     * Post-Condition: Distance is returned in inches (0 if not found)
+     * @param tagId The ID being detected
+     * @return The yaw in degrees
+     */
+    public double getYawFromId(int tagId) {
+        if (getTagBySpecificId(tagId) == null)
+            return 0;
+        else
+            return getTagBySpecificId(tagId).ftcPose.yaw;
+    }
+
+    /**
+     * Description: Checks if a tag is detected and returns a multiplier of 1 (yes) or 0 (no)
+     * Pre-Condition: All objects and hardware are initialized
+     * Post-Condition: 1 (yes) or 0 (no) is returned
+     * @param tagId The tag being checked
+     * @return The result (1 yes, 0 no)
+     */
+    public int isTagDetected(int tagId) {
+        for (AprilTagDetection detection : aprilTagDetection) {
+            if (detection.id == tagId) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
 }
